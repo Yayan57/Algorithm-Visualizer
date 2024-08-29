3929d1c5969af84eb0b8170c6f6a19442c147583
Adrian A Garza <65210505+Yayan57@users.noreply.github.com>
2024-08-28 23:44:15 -0500

    added and moved options

--- menu.py
+++ menu.py
@@ -8,7 +8,11 @@ pygame.init()
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 base_font = pygame.font.Font(None, 45)
-BG = pygame.image.load("assets/dark-gradient-background-6bly12umg2d4psr2.jpg")
+MENU_BG = pygame.image.load("assets/dark-gradient-background-6bly12umg2d4psr2.jpg")
+OPTIONS_BG = pygame.image.load("assets/white-gradient-background-o0tqqpgs66oz4rfr.jpg")
+PLAY_BG = pygame.image.load("assets/gradient-colour-background-illustration-free-vector.jpg")
+SORT_BG = pygame.image.load("assets/dark-brown-gradient-background-dark-red-brown-background-pattern-for-website-landing-pages-blurred-template-vector.jpg")
+SEARCH_BG = pygame.image.load("assets/papers.co-sk64-dark-green-blur-gradation-23-wallpaper.jpg")
 
 
 def get_font(size):
@@ -21,7 +25,7 @@ def sort_menu():
     while True:
         SORT_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("black")
+        SCREEN.blit(SORT_BG, (0, 0))
 
         SORT_TEXT = get_font(45).render("Choose a sorting algorithm", True, "#FFFFFF")
         SORT_RECT = SORT_TEXT.get_rect(center=(640, 160))
@@ -82,7 +86,7 @@ def sort_menu():
                 if SORT_SELECTION.checkForInput(SORT_MOUSE_POS):
                     return "Selection"
                 if SORT_BACK.checkForInput(SORT_MOUSE_POS):
-                    return "menu"
+                    return "play"
 
         pygame.display.update()
 
@@ -93,38 +97,43 @@ def search_menu():
     while True:
         SORT_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("white")
+        SCREEN.blit(SEARCH_BG, (0, 0))
 
-        SEARCH_TEXT = get_font(45).render("Choose a search algorithm", True, "#000000")
-        SEARCH_RECT = SEARCH_TEXT.get_rect(center=(640, 160))
+        SEARCH_TEXT = get_font(45).render("Choose a search algorithm", True, "#FFFFFF")
+        SEARCH_RECT = SEARCH_TEXT.get_rect(center=(640, 100))
         SCREEN.blit(SEARCH_TEXT, SEARCH_RECT)
 
-        SEARCH_DFS = Button(image=None, pos=(640, 260), text_input="DFS", font=get_font(75), base_color="Black",
+        INST_TEXT = get_font(45).render("Space to start, and R to reset when search is done.", True, "#FFFFFF")
+        INST_RECT = INST_TEXT.get_rect(center=(640, 160))
+        SCREEN.blit(INST_TEXT, INST_RECT)
+
+        SEARCH_DFS = Button(image=pygame.image.load("assets/dfs.png"), pos=(250, 400), text_input="", font=get_font(75), base_color="Black",
                              hovering_color="Green")
+
 
         SEARCH_DFS.changeColor(SORT_MOUSE_POS)
         SEARCH_DFS.update(SCREEN)
 
-        SEARCH_BFS = Button(image=None, pos=(640, 310), text_input="BFS", font=get_font(75),
+        SEARCH_BFS = Button(image=pygame.image.load("assets/bfs.png"), pos=(500, 400), text_input="", font=get_font(75),
                                 base_color="Black",
                                 hovering_color="Green")
 
         SEARCH_BFS.changeColor(SORT_MOUSE_POS)
         SEARCH_BFS.update(SCREEN)
 
-        SEARCH_DIJKSTRA = Button(image=None, pos=(640, 360), text_input="DIJKSTRA", font=get_font(75), base_color="Black",
+        SEARCH_DIJKSTRA = Button(image=pygame.image.load("assets/dijkstra.png"), pos=(750, 400), text_input="", font=get_font(75), base_color="Black",
                             hovering_color="Green")
 
         SEARCH_DIJKSTRA.changeColor(SORT_MOUSE_POS)
         SEARCH_DIJKSTRA.update(SCREEN)
 
-        SEARCH_ASTAR = Button(image=None, pos=(640, 410), text_input="ASTAR", font=get_font(75), base_color="Black",
+        SEARCH_ASTAR = Button(image=pygame.image.load("assets/astar.png"), pos=(1000, 400), text_input="", font=get_font(75), base_color="Black",
                             hovering_color="Green")
 
         SEARCH_ASTAR.changeColor(SORT_MOUSE_POS)
         SEARCH_ASTAR.update(SCREEN)
 
-        SEARCH_BACK = Button(image=None, pos=(640, 560), text_input="BACK", font=get_font(75),
+        SEARCH_BACK = Button(image=None, pos=(640, 650), text_input="BACK", font=get_font(75),
                            base_color="White",
                            hovering_color="Green")
 
@@ -145,7 +154,7 @@ def search_menu():
                 if SEARCH_ASTAR.checkForInput(SORT_MOUSE_POS):
                     traversal_algorithms.algorithm = "ASTAR"
                 if SEARCH_BACK.checkForInput(SORT_MOUSE_POS):
-                    return "menu"
+                    return "play"
                 return "Traversal"
 
         pygame.display.update()
@@ -159,7 +168,7 @@ def play():
 
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("black")
+        SCREEN.blit(PLAY_BG, (0, 0))
 
         PLAY_TEXT = get_font(45).render("Choose a type of algorithm", True, "#FFFFFF")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 160))
@@ -203,9 +212,13 @@ def options():
     # variables
     pygame.display.set_caption("Options")
 
-    input_box = pygame.Rect(900, 160, 140, 40)
-    input_text = ""
-    active = False
+    input_box_speed = pygame.Rect(900, 250, 140, 40)
+    input_text_speed = ""
+    active_speed = False
+
+    input_box_checkpoint_speed = pygame.Rect(900, 300, 140, 40)
+    input_text_checkpoint_speed = ""
+    active_checkpoint_speed = False
 
     MIN = 5
     MAX = 55
@@ -213,42 +226,53 @@ def options():
 
     slider = Slider((970, 225), (140, 30), .5, 0, 100)
 
+    # Slider for checkpoints (ranges from 1 to 9)
+    slider_checkpoint = Slider((970, 375), (140, 30), 1, 1, 9)
+
     COLOR_INACTIVE = (100, 80, 255)
     COLOR_ACTIVE = (100, 200, 255)
     COLOR_LIST_INACTIVE = (255, 100, 100)
     COLOR_LIST_ACTIVE = (255, 150, 150)
 
     # dropdown box
-
-    OPTIONS_THEME_COLOR = DropDown(
+    OPTIONS_THEME_COLOR_SORT = DropDown(
         [COLOR_INACTIVE, COLOR_ACTIVE],
         [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
-        900, 250, 140, 50,
+        900, 160, 140, 40,
         pygame.font.SysFont(None, 30),
-        "Select Theme", ["Normal", "Dark", "Light", "Dusk", "Spring", "Summer", "Fall", "Winter"])
+        "Select Theme", ["Normal", "Dark", "Light", "Dusk", "Spring", "Summer", "Fall", "Winter"]
+    )
 
-    current_theme = sorting_algorithm.THEME
+    OPTIONS_THEME_COLOR_SEARCH = DropDown(
+        [COLOR_INACTIVE, COLOR_ACTIVE],
+        [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
+        900, 400, 140, 40,
+        pygame.font.SysFont(None, 30),
+        "Select Theme", ["Normal", "Dark", "Light", "Dusk", "Spring", "Summer", "Fall", "Winter"]
+    )
 
-
+    current_theme_sort = sorting_algorithm.THEME
+    current_theme_search = traversal_algorithms.THEME
 
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-        SCREEN.fill("white")
-        #title
+        SCREEN.blit(OPTIONS_BG, (0, 0))
+
+        # title
         OPTIONS_TEXT = get_font(45).render("This is Options screen.", True, "#000000")
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 100))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        #FPS text
+        # FPS text
         OPTIONS_FPS = get_font(45).render(f"Speed: {sorting_algorithm.FPS}", True, "#000000")
-        OPTIONS_FPS_RECT = OPTIONS_TEXT.get_rect(center=(400, 175))
+        OPTIONS_FPS_RECT = OPTIONS_TEXT.get_rect(center=(400, 275))
         SCREEN.blit(OPTIONS_FPS, OPTIONS_FPS_RECT)
 
         # Get the rounded value from the slider
         slider_value = round(slider.get_value())
-        num_rectangles = max(MIN,
-                             min(MAX, int((slider_value / 100) * max_rectangles)))  # Scale between 5 and max_rectangles
-        #slider text
+        num_rectangles = max(MIN, min(MAX, int((slider_value / 100) * max_rectangles)))  # Scale between 5 and max_rectangles
+
+        # slider text
         OPTIONS_NRECTANGLES = get_font(45).render(f"Number of Rectangles: {num_rectangles}", True, "#000000")
         OPTIONS_NRECTANGLES_RECT = OPTIONS_TEXT.get_rect(center=(400, 225))
         SCREEN.blit(OPTIONS_NRECTANGLES, OPTIONS_NRECTANGLES_RECT)
@@ -257,78 +281,123 @@ def options():
         slider.move_slider(OPTIONS_MOUSE_POS)
         slider.render(SCREEN)
 
+        # Get the rounded value from the checkpoint slider
+        checkpoint_value = round(slider_checkpoint.get_value())
 
+        # Checkpoints text
+        OPTIONS_CHECKPOINTS = get_font(45).render(f"Number of Checkpoints: {checkpoint_value}", True, "#000000")
+        OPTIONS_CHECKPOINTS_RECT = OPTIONS_TEXT.get_rect(center=(400, 375))
+        SCREEN.blit(OPTIONS_CHECKPOINTS, OPTIONS_CHECKPOINTS_RECT)
 
-        #back button
-        OPTIONS_BACK = Button(image=None, pos=(640, 600), text_input="BACK", font=get_font(75), base_color="Black",
-                              hovering_color="Green")
+        # Checkpoints speed text
+        OPTIONS_CHECKPOINTS_SPEED = get_font(45).render(f"Traversal Speed: {traversal_algorithms.FPS}", True, "#000000")
+        OPTIONS_CHECKPOINTS_RECT_SPEED = OPTIONS_TEXT.get_rect(center=(400, 325))
+        SCREEN.blit(OPTIONS_CHECKPOINTS_SPEED, OPTIONS_CHECKPOINTS_RECT_SPEED)
+
+        # Render checkpoint slider
+        slider_checkpoint.move_slider(OPTIONS_MOUSE_POS)
+        slider_checkpoint.render(SCREEN)
+
+        # back button
+        OPTIONS_BACK = Button(image=None, pos=(640, 600), text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
 
-        text_surface = get_font(40).render(input_text, True, pygame.Color('black'))
-        text_rect = text_surface.get_rect()
+        # Render input box for speed
+        text_surface_speed = get_font(40).render(input_text_speed, True, pygame.Color('black'))
+        SCREEN.blit(text_surface_speed, input_box_speed.topleft)
+        pygame.draw.rect(SCREEN, pygame.Color('gray'), input_box_speed, 2)
 
-        if text_rect.width > input_box.width - 7:
-            text_rect.right = input_box.right - 5
-        else:
-            text_rect.right = input_box.right - 5
-        text_rect.centery = input_box.centery
+        # Render input box for checkpoints speed
+        text_surface_checkpoint_speed = get_font(40).render(input_text_checkpoint_speed, True, pygame.Color('black'))
+        SCREEN.blit(text_surface_checkpoint_speed, input_box_checkpoint_speed.topleft)
+        pygame.draw.rect(SCREEN, pygame.Color('gray'), input_box_checkpoint_speed, 2)
 
-        clip_rect = pygame.Rect(input_box.left + 5, input_box.top, input_box.width - 7, input_box.height)
-
-        pygame.draw.rect(SCREEN, pygame.Color('gray'), input_box, 2)
-        SCREEN.set_clip(clip_rect)
-        SCREEN.blit(text_surface, text_rect)
-        SCREEN.set_clip(None)
         event_list = pygame.event.get()
         for event in event_list:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     sorting_algorithm.num_rectangles = num_rectangles
-                    sorting_algorithm.THEME = current_theme
+                    sorting_algorithm.THEME = current_theme_sort
+                    traversal_algorithms.THEME = current_theme_search
+                    traversal_algorithms.num_of_checkpoints = checkpoint_value
                     return "menu"
-                if input_box.collidepoint(event.pos):
-                    active = True
+                if input_box_speed.collidepoint(event.pos):
+                    active_speed = True
+                    active_checkpoint_speed = False
+                elif input_box_checkpoint_speed.collidepoint(event.pos):
+                    active_checkpoint_speed = True
+                    active_speed = False
                 else:
-                    active = False
+                    active_speed = False
+                    active_checkpoint_speed = False
 
                 slider.check_mouse_down(OPTIONS_MOUSE_POS)
+                slider_checkpoint.check_mouse_down(OPTIONS_MOUSE_POS)
 
             if event.type == pygame.MOUSEBUTTONUP:
                 slider.check_mouse_up()
+                slider_checkpoint.check_mouse_up()
 
-            if event.type == pygame.KEYDOWN and active:
-                if event.key == pygame.K_BACKSPACE:
-                    input_text = input_text[:-1]
-                elif event.key == pygame.K_RETURN:
-                    if input_text.isdigit() and len(input_text) < 8:
-                        sorting_algorithm.FPS = int(input_text)
-                        input_text = ""
-                elif event.unicode.isdigit():
-                    if len(input_text) < 7:
-                        input_text += event.unicode
-        theme_option = OPTIONS_THEME_COLOR.update(event_list)
-        if theme_option >= 0:
-            current_theme = OPTIONS_THEME_COLOR.options[theme_option]
-            OPTIONS_THEME_COLOR.main = current_theme
-        OPTIONS_THEME_COLOR.draw(SCREEN)
+            if event.type == pygame.KEYDOWN:
+                if active_speed:
+                    if event.key == pygame.K_BACKSPACE:
+                        input_text_speed = input_text_speed[:-1]
+                    elif event.key == pygame.K_RETURN:
+                        if input_text_speed.isdigit() and len(input_text_speed) < 8:
+                            sorting_algorithm.FPS = int(input_text_speed)
+                            input_text_speed = ""
+                    elif event.unicode.isdigit():
+                        if len(input_text_speed) < 7:
+                            input_text_speed += event.unicode
+
+                if active_checkpoint_speed:
+                    if event.key == pygame.K_BACKSPACE:
+                        input_text_checkpoint_speed = input_text_checkpoint_speed[:-1]
+                    elif event.key == pygame.K_RETURN:
+                        if input_text_checkpoint_speed.isdigit() and len(input_text_checkpoint_speed) < 8:
+                            traversal_algorithms.FPS = int(input_text_checkpoint_speed)
+                            input_text_checkpoint_speed = ""
+                    elif event.unicode.isdigit():
+                        if len(input_text_checkpoint_speed) < 7:
+                            input_text_checkpoint_speed += event.unicode
+
+        theme_option_sort = OPTIONS_THEME_COLOR_SORT.update(event_list)
+        if theme_option_sort >= 0:
+            current_theme_sort = OPTIONS_THEME_COLOR_SORT.options[theme_option_sort]
+            OPTIONS_THEME_COLOR_SORT.main = current_theme_sort
+        OPTIONS_THEME_COLOR_SORT.draw(SCREEN)
 
         # theme_text
-        OPTIONS_THEME = get_font(45).render(f"Sort theme: {str(current_theme)}", True, "#000000")
-        OPTIONS_THEME_RECT = OPTIONS_TEXT.get_rect(center=(400, 275))
-        SCREEN.blit(OPTIONS_THEME, OPTIONS_THEME_RECT)
+        OPTIONS_THEME_SORT = get_font(45).render(f"Sort theme: {str(current_theme_sort)}", True, "#000000")
+        OPTIONS_THEME_SORT_RECT = OPTIONS_TEXT.get_rect(center=(400, 175))
+        SCREEN.blit(OPTIONS_THEME_SORT, OPTIONS_THEME_SORT_RECT)
+
+        theme_option_search = OPTIONS_THEME_COLOR_SEARCH.update(event_list)
+        if theme_option_search >= 0:
+            current_theme_search = OPTIONS_THEME_COLOR_SEARCH.options[theme_option_search]
+            OPTIONS_THEME_COLOR_SEARCH.main = current_theme_search
+        OPTIONS_THEME_COLOR_SEARCH.draw(SCREEN)
+
+        # theme_text
+        OPTIONS_THEME_SEARCH = get_font(45).render(f"Sort theme: {str(current_theme_search)}", True, "#000000")
+        OPTIONS_THEME_SEARCH_RECT = OPTIONS_TEXT.get_rect(center=(400, 400))
+        SCREEN.blit(OPTIONS_THEME_SEARCH, OPTIONS_THEME_SEARCH_RECT)
 
         pygame.display.flip()
+
+
 
 
 def main_menu():
     pygame.display.set_caption("Menu")
 
     while True:
-        SCREEN.blit(BG, (0, 0))
+        SCREEN.blit(MENU_BG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
